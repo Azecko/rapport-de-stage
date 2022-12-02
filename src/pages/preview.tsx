596da525102af4d76d62ef32f111c39c74b56ba2
Azecko <sami.panseri@epfl.ch>
2022-12-02 15:32:06 +0100

    [fix] DateHelper when empty

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -16,6 +16,7 @@ const Container = styled.div``
  * will work, but might be too long to fit in the field.
  */
 const dateHelper = (strdate: string) => {
+  if (!strdate) return ''
   const date = new Date(strdate)
   const formatter = new Intl.DateTimeFormat('fr', { year: 'numeric', month: 'long', day: 'numeric' })
   return formatter.format(date)
