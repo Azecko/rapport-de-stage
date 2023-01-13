a3d95c64bb6fbd62380e5951e4fd8b5fcc6183c8
Nicolas Borboën <ponsfrilus@gmail.com>
2023-01-13 10:33:52 +0100

    [fix] conflicts

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -16,8 +16,8 @@ export default function HomePage () {
         <h1>Rapport de stage</h1>
       </Box>
       <div key="divForm">
-          <button onClick={() => navigate('/stagiaire')}>Stagiaire</button>
-          <button onClick={() => navigate('/responsable')}>Responsable</button>
+        <button onClick={() => navigate('/stagiaire')}>Stagiaire</button>
+        <button onClick={() => navigate('/responsable')}>Responsable</button>
       </div>
     </Container>
   )
