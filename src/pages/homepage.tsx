f1b00eb794e38523dec0f6a0dff95fcfdd43bab5
Azecko <sami.panseri@epfl.ch>
2023-01-18 09:12:08 +0100

    [feat] better homepage
    Centered, description, buttons..

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {
-  Box
+  Box, Button, Tooltip
 } from '@mui/material'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
+import '../style/homepage.css'
 const Container = styled.div``
 
 export default function HomePage () {
@@ -12,13 +13,37 @@ export default function HomePage () {
 
   return (
     <Container>
-      <Box>
-        <h1>Rapport de stage</h1>
-      </Box>
-      <div key="divForm">
-          <button onClick={() => navigate('/stagiaire')}>Stagiaire</button>
-          <button onClick={() => navigate('/responsable')}>Responsable</button>
+      <div key="divForm" className="homepageDiv">
+        <Box>
+            <h1>Rapport de stage</h1>
+            <p>
+              <a
+                href="https://github.com/ponsfrilus/rapport-de-stage"
+                target="_blank"
+                rel="noreferrer">
+                  rapport-de-stage
+              </a> est un outil permettant de générer des <a
+              href="https://www.vd.ch/themes/formation/orientation/faire-des-stages#c2033329"
+              target="_blank"
+              rel="noreferrer">
+                rapports de stages officiels
+              </a>  du canton de Vaud pour les stagiaires et les responsables de stages.
+              <h3>Fonctionnalités :</h3>
+              <li>Rapports officiels et valides</li>
+              <li>Imprimable</li>
+              <li>Maintien des données après avoir sauvegardé via le stockage local</li>
+            </p>
+            <div className="homepageButtons">
+              <Tooltip title="Créer un rapport de stage Stagiaire" arrow>
+                <Button variant='contained' onClick={() => navigate('/stagiaire')}>Stagiaire</Button>
+              </Tooltip>
+              <Tooltip title="Créer un rapport de stage Responsable" arrow>
+                <Button variant='contained' onClick={() => navigate('/responsable')}>Responsable</Button>
+              </Tooltip>
+            </div>
+        </Box>
       </div>
+      <footer className="homepageFooter"><p> Coded with 💙 by <a href="https://github.com/Azecko" target="_blank" rel="noreferrer">Azecko</a></p></footer>
     </Container>
   )
 }
