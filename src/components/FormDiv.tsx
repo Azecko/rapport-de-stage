db763f2d9ece7229e4f586eb997577fe3e01b8cf
Azecko <sami.panseri@epfl.ch>
2023-02-14 11:24:10 +0100

    [fix] NoVar eslint

--- src/components/FormDiv.tsx
+++ src/components/FormDiv.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 /* eslint-disable no-case-declarations */
 /* eslint-disable react/react-in-jsx-scope */
 import { Box, Checkbox, FormControlLabel, FormGroup, Radio, RadioGroup, TextField } from '@mui/material'
